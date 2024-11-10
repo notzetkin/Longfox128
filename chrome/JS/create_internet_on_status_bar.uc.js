9dbcc8d3f41c1cc7a717a22959440587d10cebcb
notzetkin <86797505+notzetkin@users.noreply.github.com>
2024-11-11 00:44:04 +0300

    Delete chrome/JS directory

--- chrome/JS/create_internet_on_status_bar.uc.js
+++
@@ -1,8 +0,0 @@
-var internet = document.createElement("div");
-var statusBarInner = document.createElement("div");
-statusBarInner.classList.add('statusBarInner');
-internet.classList.add('statusBarInternet');
-document.getElementById('statuspanel').appendChild(statusBarInner);
-document.getElementById('statuspanel').appendChild(statusBarInner.cloneNode(true)).classList.add('statusBarInnerTwoBoxes');
-document.getElementById('statuspanel').appendChild(statusBarInner.cloneNode(true)).classList.add('statusBarInnerTwoBoxes');
-document.getElementById('statuspanel').appendChild(internet);
